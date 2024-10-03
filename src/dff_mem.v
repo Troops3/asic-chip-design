e41af6ba83225b801aad4457d375171f94a741ea
Eshaan Mehta <eshaanm.mehta@gmail.com>
2024-10-03 19:41:41 -0400

    Update dff_mem.v

--- src/dff_mem.v
+++ src/dff_mem.v
@@ -8,7 +8,15 @@ module dff_mem #(
     output wire [7:0] data_out,  
     input  wire       clk,   
     input  wire       rin,     
-    input  wire       rout_n     
+    input  wire       rout_n,
+
+    input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
+    output reg  [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
+    input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
+    output wire [7:0] uio_out,  // IOs: Bidirectional Output path
+    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // will go high when the design is enabled
+    input  wire       rst_n     // reset_n - low to reset
 );
 
   localparam addr_bits = $clog2(RAM_BYTES);
