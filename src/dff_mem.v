0360be438902b2795e1c9c3ef43a34dbc66dc176
Darius Rudaitis <81165464+Troops3@users.noreply.github.com>
2024-10-03 21:08:32 -0400

    free 25

--- src/dff_mem.v
+++ src/dff_mem.v
@@ -3,13 +3,6 @@
 module tt_um_dff_mem #(
     parameter RAM_BYTES = 16
 ) (
-    input wire [3:0] addr_in,
-    input wire [7:0] data_in,
-    output wire [7:0] data_out,  
-    input  wire       clk,   
-    input  wire       rin,     
-    input  wire       rout_n,
-
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output reg  [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
@@ -18,22 +11,25 @@ module tt_um_dff_mem #(
     input  wire       ena,      // will go high when the design is enabled
     input  wire       rst_n     // reset_n - low to reset
 );
+    localparam addr_bits = 4;
 
   wire _unused = &{ena, rst_n, ui_in, uo_out, uio_in, uio_out, uio_oe};
     
-  wire wr_en = ~rout_n;
-  assign uio_oe  = 8'b0;  // All bidirectional IOs are inputs
-  assign uio_out = 8'b0;
+  wire [3:0] addr = ui_in[3:0];
+  wire wr_en = ui_in[7];
+  wire r_en = ui_in[6];
+  // assign uio_oe  = 8'b0;  // All bidirectional IOs are inputs
+  // assign uio_out = 8'b0;
 
   reg [7:0] RAM[RAM_BYTES - 1:0];
 
   always @(posedge clk) begin
       // case 1: write to ram
-      if (wr_en && ~rin) begin
-          RAM[addr_in] <= data_in; 
-      end else if (rin && ~wr_en) begin
+      if (wr_en && ~r_en) begin
+          RAM[addr] <= uio_in; 
+      end else if (r_en && ~wr_en) begin
       // case 2: read to ram
-          data_out <= RAM[addr_in];
+          uio_out <= RAM[addr];
       end else begin
           // case 3: conflict, do nothing
       end
