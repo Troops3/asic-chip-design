0cc1c614315a353b511619dd7eed4116444da90a
Darius Rudaitis <81165464+Troops3@users.noreply.github.com>
2024-10-07 11:48:34 -0400

    Update dff_mem.v

--- src/dff_mem.v
+++ src/dff_mem.v
@@ -3,21 +3,22 @@
 module tt_um_dff_mem #(
     parameter RAM_BYTES = 16
 ) (
-    input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
-    output reg  [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
-    input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
-    output wire [7:0] uio_out,  // IOs: Bidirectional Output path
-    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
+    input  wire [7:0] ui_in,    // Dedicated inputs - address
+    output reg  [7:0] uo_out,   // Dedicated outputs - not used
+    input  wire [7:0] uio_in,   // IOs: write data
+    output reg [7:0] uio_out,  // IOs: read data
+    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output), not used
     input  wire       ena,      // will go high when the design is enabled
     input  wire       rst_n,     // reset_n - low to reset
-    input  wire       clk
+    input  wire       clk        //for clock sychroniztion
 );
     localparam addr_bits = 4;
     
   wire [3:0] addr = ui_in[3:0];
   wire wr_en = ui_in[7];
   wire r_en = ui_in[6];
-  // assign uio_oe  = 8'b0;  // All bidirectional IOs are inputs
+    
+  // assign uio_oe  = 8'b0;  
   // assign uio_out = 8'b0;
 
   reg [7:0] RAM[RAM_BYTES - 1:0];
